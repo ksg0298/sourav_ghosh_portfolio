9d666ecff389551b9012ce51ad4dcbd9bd688430
Sourav Ghosh <sg.sourav0298.com>
2024-11-13 00:04:39 +0530

    changes upto 21:42

---
+++ components/Header.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { Button } from './ui/button'
+import React from 'react'
+import Nav from './Nav'
+
+const Header = () => {
+  return (
+    <header className='py-8 xl:py-12 text-white '>
+        <div className='container mx-auto flex justify-between items-center'> 
+            {/* Logo */}
+            <Link href={"/"}>
+            <h1 className='text-4xl font-semibold'>
+                Sourav <span className='text-accent'>.</span>
+            </h1>
+            </Link>
+            {/* desktop nav & Hire me button*/}
+            <div className="hidden md:flex items-center gap-8">
+             <Nav/>
+             <Link href={"/contact"}>
+                <Button>Hire Me!</Button>
+             </Link>
+            </div>
+            {/* mobile nav */}
+            <div className='md:hidden'>Mobile nav</div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
